4ca6e8bbbbf1bc9ab6686953f1de797e03e3aa6f
vandat-dev <datbe26092001@gmail.com>
2025-11-22 17:50:28 +0700

    fix: update 1 jenkins

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ app = FastAPI()
 
 @app.get("/")
 def root():
+    host = os.getenv("HOST", "0.0.0.0")
     port = int(os.getenv("PORT", "8000"))
-    return {"message": "Hello Jenkins on port " + str(port)}
+    return {"message": "Hello Jenkins on " + host + str(port) + "!"}
 
 def main():
     host = os.getenv("HOST", "0.0.0.0")
